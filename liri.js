ad581498fc5a378584c345b9e2d3e98607e63d2e
Mai Houa Vue <mhvue4@gmail.com>
2020-02-16 19:25:09 -0600

    organized code, edit func do-what-it-says to work

--- liri.js
+++ liri.js
@@ -9,8 +9,8 @@ var fs = require("fs");
 //gather input 
 var nodeArgs = process.argv;
 var action = process.argv[2]; //ex: concert-this or movie-this goes in this index
-// var search = process.argv[3] //user input value
 var search = "";
+
 for (var i = 3; i < nodeArgs.length; i++) {
     if(i > 3 && i < nodeArgs.length){
         search = search + " " + nodeArgs[i]; 
@@ -19,7 +19,6 @@ for (var i = 3; i < nodeArgs.length; i++) {
     search += nodeArgs[i];
     }
 }
-console.log(search);
 // }
 
 //to get user's input and know which category to start which funct
@@ -33,82 +32,87 @@ else if(action === "movie-this") {
     movieSearch();
 }
 else if(action ==="do-what-it-says"){
-    doWhat();
+    heyLiri();
 }
 else {
-    console.log("Sorry. Please try again. Don't forget to include category first. Ex: movie-this <input name of movie>")
+    console.log("Please try again. Don't forget to include category first. Ex: movie-this <input name of movie>")
 }
 
 
 //concert-this <artist/band name here>
 function concertSearch(){
-
     var queryUrl= "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
-
     axios
     .get(queryUrl).then(function(response) {
-    // console.log(response.data[1].datetime);
-    for(var j = 0; j < response.data.length; j++) {
+    for(var j = 0; j < 5; j++) {
 
-    var venue = response.data[j].venue.name;
-    var concertCity = response.data[j].venue.city;
-    var concertCountry = response.data[j].venue.country;
-    var dateResponse = response.data[1].datetime
-    var dateOnly= dateResponse.slice(0,10);
-    var convertDate= moment(dateOnly).format("MM/DD/YYYY")
+        var concertInfo = {
+            venue: response.data[j].venue.name,
+            city: response.data[j].venue.city,
+            country: response.data[j].venue.country
+        }
+        var dateResponse = response.data[1].datetime;
+        var dateOnly= dateResponse.slice(0,10);
+        var convertDate= moment(dateOnly).format("MM/DD/YYYY");
 
-    console.log("---------------Concert Information---------------");
-    console.log("Name of Venue:" + venue
-    + "\nName of city and county: " + concertCity +", "+  concertCountry
-    + "\nDate of event: " + convertDate);
-
+        console.log("---------------Concert Information---------------");
+        console.log("Name of Venue: " + concertInfo.venue
+        + "\nName of city and county: " + concertInfo.city +", "+  concertInfo.country
+        + "\nDate of event: " + convertDate);
+        
     }
     })
     .catch(function(error) {
-    if(error.response) {
-    console.log(error.response.data);
-    } 
-    else if(error.request) {
-    console.log(error.request);
-    }
-    else {
-    console.log("Error", error.message);
-    }
+        if(error.response) {
+        console.log(error.response.data);
+        } 
+        else if(error.request) {
+        console.log(error.request);
+        }
+        else {
+        console.log("Error", error.message);
+        }
 })};
 
 //spotify-this-song '<song name here>'`
 function spotifySearch() {
-   
-    if(search === undefined) {
-        search = "The Sign by Ace of Base";
-    }
-    else{
     spotify
     .search({type:'track', query: search, limit: 5}, function(error, data){
+
+        for (var k = 0; k < data.tracks.items.length; k++) {
+            var songInfo = {
+                artistName: data.tracks.items[k].album.artists[0].name,
+                albumName: data.tracks.items[k].album.name,
+                songName: data.tracks.items[k].name,
+                previewLink: data.tracks.items[k].preview_url
+            }
+    
         if (error) {
             return console.log('Error occurred: ' + error);
         }
-        else{
-            for (var k = 0; k < data.tracks.items.length; k++) {
-            var artistName= data.tracks.items[k].album.artists[0].name;
-            var albumName=data.tracks.items[k].album.name;
-            var songName =data.tracks.items[k].name;
-
-            console.log("\nSong: " + songName + "." + "\nBy: " + 
-            artistName + "." + "\nFrom album titled: " + albumName);
-
-            var previewLink = data.tracks.items[k].preview_url;
-                if (previewLink === null)
-                    console.log("Listen preview here: ***Sorry no link at this time.***")
-                else{
-                    console.log("Listen preview here: " + previewLink);
-                }
-            
         
+        if(search = "") {
+            search = "The Sign";
+            
+            console.log("\nSong: " + songInfo.songName + "." + "\nBy: " + 
+            songInfo.artistName + "." + "\nFrom album titled: " + songInfo.albumName);;
+            console.log("Listen preview here: " + songInfo.previewLink);
+        
+    }
+        else{
+            console.log("\nSong: " + songInfo.songName + "." + "\nBy: " + 
+            songInfo.artistName + "." + "\nFrom album titled: " + songInfo.albumName);
+            if (songInfo.previewLink === null) {
+                console.log("Listen preview here: ***Sorry no link at this time.***")
+            }
+            else{
+                console.log("Listen preview here: " + songInfo.previewLink);
+            }
+            
         }
-        }
+    }
 
-})}};
+})};
 
 function movieSearch() {
     
@@ -126,14 +130,26 @@ function movieSearch() {
             actors: response.data.Actors
         }
 
-        var ratings =  {
+        var ratings = {
            ombd: response.data.Ratings[0].Source,
            ombdVal: response.data.Ratings[0].Value,
            rottonT: response.data.Ratings[1].Source,
            rottonTVal: response.data.Ratings[1].Value
         } 
-        
 
+        if(search = null) {
+            search = "Mr. Nobody";
+
+            console.log("\nMovie Name: " + movieInfo.title
+            + "\nYear Made: " + movieInfo.year
+            + "\nOMBD Ratings: " + ratings.ombd + " = " + ratings.ombdVal
+            + "\nRotten Tomatoes Ratings: " + ratings.rottonT + " = " + ratings.rottonTVal
+            + "\nProduced in: " + movieInfo.countryProd
+            + "\nLanguage of movie: " + movieInfo.language 
+            + "\nPlot: " + movieInfo.plot
+            + "\nActors: " + movieInfo.actors);
+        }
+        else {
         console.log("\nMovie Name: " + movieInfo.title
         + "\nYear Made: " + movieInfo.year
         + "\nOMBD Ratings: " + ratings.ombd + " = " + ratings.ombdVal
@@ -142,6 +158,7 @@ function movieSearch() {
         + "\nLanguage of movie: " + movieInfo.language 
         + "\nPlot: " + movieInfo.plot
         + "\nActors: " + movieInfo.actors);
+        }
     })
     .catch(function(error) {
         if(error.response) {
@@ -156,37 +173,26 @@ function movieSearch() {
 })
 };
 
-function doWhat() {
+function heyLiri() {
     fs.readFile("random.txt", "utf8", function(error, data) {
+        var dataNew = data.split(",")
+        action = dataNew[0]
+        search = dataNew[1];
+        
+        if(action === "spotify-this-song") {
+            spotifySearch();
+        }
 
-            var dataNew = data.split(",")
-            action = dataNew[0]
-            search = dataNew[1];
-           
-
-            if(action === "spotify-this-song") {
-                spotifySearch();
-            }
-        // }
-        // else{
-        //     console.log("not working")
-        // }
-        // else if(action === "spotify-this-song"){
-        //     spotifySearch();
-        // }
-        // else if(action === "movie-this") {
+        // if(action === "movie-this"){ //testing this out 
         //     movieSearch();
         // }
-        // else if(action ==="do-what-it-says"){
-        //     doWhat();
+        // console.log(action,search)
+        // if(action === "concert-this") {
+        //     concertSearch();
         // }
-        // else {
-        //     console.log("Sorry. Please try again. Don't forget to include category first. Ex: movie-this <input name of movie>")
-        // }
-    
-        // if (error) {
-        //     return console.log(error);
-        //   }
+        if(error) { 
+            console.log(error)
+        }
         
-    })
+    });
 }
